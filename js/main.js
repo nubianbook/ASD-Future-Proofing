049c712ae00b526ed5274bdd49b65b9a72a1693c
nubianbook <nubianbook@me.com>
2012-04-30 15:44:04 -0400

    I am setting up my repository for my ASD class for the first time with my files from my previous class MIU

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+
+var parseMyForm = function(data){
+	//use form data here:
+	console.log(data);
+};
+
+$(document).ready(function(){
+	var p3form = $('#myform');
+	p3form.validate({
+		invalidHandler: function(form, validator){},
+		submithandler:	function(){
+			 var data = p3form.serializeArray();
+			 parseMyForm(data);
+		}
+		
+	});
+	
+	$("#submit")
+			.after("<input type='submit' value='Save Form'    						id='saveData'>");
+	$("#saveData")
+			.click(function(e) {
+			//prevent default of submitting form
+				e.preventDefault();
+				localStorage.setItem("flag", "set");
+				var data = $("#myform").serializeArray();
+				$.each(data, function(i, obj) {
+				     localStorage.setItem(obj.name, obj.value);
+				
+			});
+	 });
+	 
+	 if(localStorage.getItem("flag") == "set") {
+	 	$("header").before("<p>Your data has been saved</p>");
+	 	var data = $("#myform").serializeArray();
+				$.each(data, function(i, obj) {
+				   $("[name='" + obj.name 			 								+"']").val(localStorage.getItem(obj.name));
+				
+			});
+	 
+	 }
+				
+});
+
+
+
+		
+
+
+	
