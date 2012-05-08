34339591ddaf8aaf2803716968cfa4820324b560
nubianbook <nubianbook@me.com>
2012-05-08 04:42:05 -0400

    I am updating my xml file to a more rational approach, as well as adding structure to how my pages index plus a few other test and trials. At least the xml file is working on the page.

--- js/main.js
+++ js/main.js
@@ -16,30 +16,7 @@ $(document).ready(function(){
 		
 	});
 	
-	$("#submit")
-			.after("<input type='submit' value='Save Form' id='saveData'>");
-	$("#saveData")
-			.click(function(e) {
-			//prevent default of submitting form
-				e.preventDefault();
-				localStorage.setItem("flag", "set");
-				var data = $("#myform").serializeArray();
-				$.each(data, function(i, obj) {
-				     localStorage.setItem(obj.name, obj.value);
-				
-			});
-	 });
 	 
-	 if(localStorage.getItem("flag") == "set") {
-	 	$("header").before("<p>Your data has been saved</p>");
-	 	var data = $("#myform").serializeArray();
-				$.each(data, function(i, obj) {
-				   $("[name='" + obj.name 			 								+"']").val(localStorage.getItem(obj.name));
-				
-			});
-	 
-	 }
-				
 });
 
 
