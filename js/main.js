7d0ffcb9f53c0dc7a6759abae670447864c9a393
nubianbook <nubianbook@me.com>
2012-05-10 04:28:00 -0400

    I now have my XML/Ajax file working properly. Its not pretty but it works

--- js/main.js
+++ js/main.js
@@ -32,3 +32,52 @@
      });
  });
 
+ /* this is the beginning of my XML pg */
+
+ $('#infoPage').live('pageinit', function(){
+     console.log("We are on the infoPage");
+
+     $('#xmlBtn').bind('click', function(){
+         console.log("We're here");
+
+         $.ajax({
+             url: 'xhr/data.xml',
+             type: 'GET',
+             dataType: 'xml',
+             success: function(info){
+                 console.log(info);
+                 $(info).find("info").each(function(){
+                     var group = $(this).find('group').text();
+                     var movName = $(this).find('movName').text();
+                     var year = $(this).find('year').text();
+                     var score = $(this).find('score').text();
+                     var awards = $(this).find('awards').text();
+                     var country = $(this).find('country').text();
+                     var rating = $(this).find('rating').text();
+                     var notes = $(this).find('notes').text();
+                     console.log(info);
+                     $('#archive1').append($(' ' +
+                         '<img src="images/cta_logo.png" alt="photo" class="picDefault" />' +
+                         '<ul id="xmlList">' +
+                         '<li>Group: ' + group + '</li>' +
+                         '<li>MovName: ' + movName + '</li>' +
+                         '<li>Year: ' + year + '</li>' +
+                         '<li>Score: ' + score + '</li> ' +
+                         '<li>Awards: ' + awards + '</li> ' +
+                         '<li>Country: ' + country + '</li> ' +
+                         '<li>Rating: ' + rating + '</li> ' +
+                         '<li>Notes: ' + notes + '</li></ul><br/><hr/><br/>'));
+
+
+                 });
+         $('xmlList').listview('refresh');
+         console.log($('#xmlList'));
+             }
+
+         });
+
+     })
+
+ });
+
+
