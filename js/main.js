2c580131bcd3a1e9c7b1f6998051ca8cca1afe4c
nubianbook <nubianbook@me.com>
2012-05-09 21:29:28 -0400

    I have finally again got my JSON/Ajax setup to work finally

--- js/main.js
+++ js/main.js
@@ -1,49 +1,34 @@
+ $('#infoPage').live('pageinit', function(){
+    console.log("We are on the infoPage");
 
- /*
-$(function(){
+    $('#jsonBtn').bind('click', function(){
+        console.log("We're here");
 
-    $.ajax({
-
-        url: 'xhr/json.js',
-        type: 'Get',
-        dataType: 'json',
-        success: function(response){
-            for(var i=0, j=response.archive1.length; i<j; i++){
-                    var archive1 = response.archive1[1];
-            }
-            console.log(response);
-
-        };
-
-        }
-
-    });
-
-});
-  */
-
-
-    $.ajax({
-        url: 'xhr/data.json',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data){
-            for (var i=0, j=data.jsonData.length; i<j; i++){
-                var item = data.jsonData[i];
-                $('#archive1').append(
-                    '<img src="images/cta_logo.png" alt="photo" class="picDefault" />' +
-                        '<li>Groups: ' + item.groups + '</li>' +
-                        '<li>Atitle: ' + item.atitle + '</li>' +
-                        '<li>Watched: ' + item.watched + '</li>' +
-                        '<li>Checkbox1: ' + item.checkbox1 + '</li>' +
-                        '<li>Checkbox2: ' + item.checkbox2 + '</li> ' +
-                        '<li>Acquired: ' + item.acquired + '</li> ' +
-                        '<li>Viewed: ' + item.viewed + '</li> ' +
-                        '<li>RateStars: ' + item.rateStars + '</li> ' +
-                        '<li>Slider: ' + item.slider + '</li> ' +
-                        '<li>Notes: ' + item.notes + '</li><br/><hr/><br/>');
-            }
-
-        }
-});
+        $.ajax({
+            url: 'xhr/data.json',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+                 for (var i=0, j=data.jsonData.length; i<j; i++){
+                     var item = data.jsonData[i];
+                     console.log(item);
+                     $('#archive1').append($(' ' +
+                         '<img src="images/cta_logo.png" alt="photo" class="picDefault" />' +
+                             '<ul id="jsonList">' +
+                             '<li>Groups: ' + item.groups + '</li>' +
+                             '<li>Atitle: ' + item.atitle + '</li>' +
+                             '<li>Watched: ' + item.watched + '</li>' +
+                             '<li>Acquired: ' + item.acquired + '</li> ' +
+                             '<li>Viewed: ' + item.viewed + '</li> ' +
+                             '<li>RateStars: ' + item.rateStars + '</li> ' +
+                             '<li>Slider: ' + item.slider + '</li> ' +
+                             '<li>Notes: ' + item.notes + '</li></ul><br/><hr/><br/>'));
+                 }
+             }
+         });
+        $('jsonList').listview('refresh');
+        console.log($('#jsonList'));
+     });
+ });
 
